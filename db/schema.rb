b8fbdeb398ee872118a2870537d5e7c0d28fca99
Álvaro Molina <abusmolina@gmail.com>
2019-06-21 07:41:23 -0400

    ADD Area scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_21_114005) do
+ActiveRecord::Schema.define(version: 2019_06_21_114048) do
+
+  create_table "areas", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
